1190c894b023cc41f6090d2fab2cfd5e802c34ad
Johnson Vo <johnson.vo24@mail.utoronto.ca>
2021-07-05 16:13:07 -0400

    Updated SummaryByDays and SummaryByTickers

--- src/services/SenateDBFormatter.js
+++ src/services/SenateDBFormatter.js
@@ -1,5 +1,4 @@
 // Function used to grab an array of all senators
-
 export const organizeBySenator = data => {
     const senatorKey = []; 
     for (var i in data) {
@@ -8,6 +7,25 @@ export const organizeBySenator = data => {
     const senatorArr = senatorKey.filter((item, i, ar) => ar.indexOf(item) === i);
     //console.log(senatorArr);
     return (senatorArr);
+}
+// Function used to grab an array of all tickers
+export const organizeByTicker = data => {
+    const tickerKey = [];
+    for (var i in data) {
+        tickerKey.push(data[i].ticker);
+    }
+    const tickerArr = tickerKey.filter((item, i , ar) => ar.indexOf(item) === i);
+    return (tickerArr);
+}
+
+// Function used to grab an array of all tickers
+export const organizeByDays = data => {
+    const dayKey = [];
+    for (var i in data) {
+        dayKey.push(data[i].transaction_date);
+    }
+    const dayArr = dayKey.filter((item, i , ar) => ar.indexOf(item) === i);
+    return (dayArr);
 }
 
 export const getSenatorDetails = senatorName => {
